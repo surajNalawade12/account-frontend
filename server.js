ea1f9b1b26922f0fee9d12acb9764a6370f1724b
surajNalawade <suraj.n@amazatic.com>
2018-07-04 17:26:45 +0530

    update package.json

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+console.log('directory => ', __dirname);
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8000);
 
 app.get('/*', function(req, res) {
+    console.log('full path => ', path.join(__dirname + 'index.html'));
   res.sendFile(path.join(__dirname + 'index.html'));
 });
 
