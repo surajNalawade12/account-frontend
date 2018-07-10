9d95ba1359ebee6c5bf54ea77d511f722ca4688d
surajNalawade <suraj.n@amazatic.com>
2018-07-10 10:50:33 +0530

    update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8000);
-console.log('');
 app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/src/index.html');
 });
